b4927e6d6dd47109a05ca777cfe63f239db58227
Bill Jones <misterunix@gmail.com>
2023-09-27 19:59:53 -0500

    more build

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-go build -o bin/wordlistidea
 
-scp bin/wordlistidea bjones@i3-1:~/wordlistidea
+GOOS=linux GOARCH=amd64 go build -o bin/wordlistidea-linux-amd64
+GOOS=windows GOARCH=amd64 go build -o bin/wordlistidea-windows-amd64.exe
+
+scp bin/wordlistidea-linux-amd64 bjones@i3-1:~/wordlistidea-linux-amd64
